008a6fcb8024bef6b964895e900ea7f21f5865dd
yansheng666 <40790190+yansheng666@users.noreply.github.com>
2022-12-20 14:00:58 +0800

    Create sgwa.js

---
+++ sgwa.js
@@ -0,0 +1,38 @@
+
+/**
+ 项目名称：水果文案
+ 有问题联系作者微信：yushouyu233
+ 使用说明：此脚本仅供学习与交流.
+         请勿转载与贩卖！
+ 
+[rewrite_local]
+^https:\/\/mmm\.mirzibaba\.com\/api\/user url script-response-body
+yansheng.js
+[mitm] 
+hostname = mmm.mirzibaba.com
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const yansheng = '/getUserInfo';
+const yansheng1 = '/copyImgArticle';
+
+
+if (url.indexOf(yansheng) != -1){
+    obj.data.isVip = 1;
+    obj.data.vipEndTime = "大佬，恭喜您成为尊贵的vip";
+    obj.data.downloadNumber = 1000;
+    body = JSON.stringify(obj);
+}
+
+if (url.indexOf(yansheng1) != -1){
+    obj.data.isVip = 1;
+    obj.data.downloadNumber = 997;
+    //obj.data.downloaded = 997;
+    body = JSON.stringify(obj);
+}
+
+
+$done({body});
