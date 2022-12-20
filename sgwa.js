5909a537d61bb64bc5568db77865b6ec28d98912
yansheng666 <40790190+yansheng666@users.noreply.github.com>
2022-12-20 14:50:20 +0800

    Update sgwa.js

--- sgwa.js
+++ sgwa.js
@@ -1,14 +1,20 @@
-**
+/******************************
+
  项目名称：水果文案
  有问题联系作者微信：yushouyu2333
  使用说明：此脚本仅供学习与交流.
          请勿转载与贩卖！
- 
+ ⚠️⚠️⚠️
+
+*******************************
+
+
 [rewrite_local]
-^https:\/\/mmm\.mirzibaba\.com\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/sgwa.js
+^https:\/\/mmm\.mirzibaba\.com\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/sgwa.js.js
 [mitm] 
 hostname = mmm.mirzibaba.com
-*/
+
+*******************************/
 
 var body = $response.body;
 var url = $request.url;
@@ -16,7 +22,6 @@ var obj = JSON.parse(body);
 
 const yansheng = '/getUserInfo';
 const yansheng1 = '/copyImgArticle';
-
 
 if (url.indexOf(yansheng) != -1){
     obj.data.isVip = 1;
@@ -32,6 +37,4 @@ if (url.indexOf(yansheng1) != -1){
     body = JSON.stringify(obj);
 }
 
-
 $done({body});
-
