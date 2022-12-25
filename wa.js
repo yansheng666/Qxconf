5fca85d7aa73e10fee6f5bb4c6fbe461d34a1815
yansheng666 <40790190+yansheng666@users.noreply.github.com>
2022-12-25 10:53:20 +0800

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,40 +1,20 @@
-/******************************
-
- 项目名称：水果文案
+#/*****************
+ 
+ 项目名称：文案合集
  有问题联系作者微信：yushouyu2333
  使用说明：此脚本仅供学习与交流.
          请勿转载与贩卖！
- ⚠️⚠️⚠️
+         ⚠️⚠️⚠️
 
-*******************************
+#*******************/
 
-
+#水果文案
 [rewrite_local]
 ^https:\/\/mmm\.mirzibaba\.com\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/sgwa.js
+
+rewrite_local]
+^https:\/\/leyou\.jingtanwl\.cn\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/sgwa.js
+
+
 [mitm] 
-hostname = mmm.mirzibaba.com
-
-*******************************/
-
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const yansheng = '/getUserInfo';
-const yansheng1 = '/copyImgArticle';
-
-if (url.indexOf(yansheng) != -1){
-    obj.data.isVip = 1;
-    obj.data.vipEndTime = "大佬，恭喜您成为尊贵的vip";
-    obj.data.downloadNumber = 1000;
-    body = JSON.stringify(obj);
-}
-
-if (url.indexOf(yansheng1) != -1){
-    obj.data.isVip = 1;
-    obj.data.downloadNumber = 997;
-    //obj.data.downloaded = 997;
-    body = JSON.stringify(obj);
-}
-
-$done({body});
+hostname = mmm.mirzibaba.com,leyou.jingtanwl.cn
