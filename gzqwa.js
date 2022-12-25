0e0167d8169413d53afdcdf7f05940768c6b235b
yansheng666 <40790190+yansheng666@users.noreply.github.com>
2022-12-25 13:33:12 +0800

    Update gzqwa.js

--- gzqwa.js
+++ gzqwa.js
@@ -27,13 +27,11 @@ if (url.indexOf(yansheng) != -1){
     obj.data.vipEndTime = "大佬，恭喜您成为尊贵的vip";
     body = JSON.stringify(obj);
 }
-
 if (url.indexOf(fz) != -1){
     obj.data.can = 1;
     obj.data.code = 1;
     body = JSON.stringify(obj);
 }
-
 if (url.indexOf(bc) != -1){
     obj.data.can = 1;
     obj.data.code = 1;
