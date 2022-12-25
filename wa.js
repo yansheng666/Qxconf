f6bd1b2c543a8577e6acc0599821756b73ff54fc
yansheng666 <40790190+yansheng666@users.noreply.github.com>
2022-12-25 14:15:08 +0800

    Update wa.js

--- wa.js
+++ wa.js
@@ -14,6 +14,3 @@
 #乐优文案
 rewrite_local]
 ^https:\/\/leyou\.jingtanwl\.cn\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/sgwa.js
-#果之圈文案
-rewrite_local]
-^https:\/\/gzq\.jingtanwl\.cn\/api\/user url script-response-body https://raw.githubusercontent.com/yansheng666/Qxconf/main/gzqwa.js
